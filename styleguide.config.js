d9e8a00d6843a868a8f67e3f1b9be782ecc690eb
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2019-07-18 13:49:01 -0700

    Feat/initial (#2)
    * Initial commit
    
    * add styleguidist
    
    * replace content in /dist
    
    * add UMD output
    
    * move styleguidist build folder. Rename package.json script names
    
    * allow components without example on styleguidist
    
    * update buildspec
    
    * delete /dist
    
    * update .gitignore to ignore dist
    
    * add prepare command
    
    * add EOF to  dot-files
    
    * trigger codebuild
    
    * add CI to buildspec
    
    * fix typo in buildspec
    
    * trigger build
    
    * temp fix to pass codebuild check

---
+++ styleguide.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  template: {
+    favicon: 'https://dashboard.runpanther.io/favicon.ico',
+  },
+  title: 'Pounce UI Docs',
+  usageMode: 'expand',
+  propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json').parse,
+  components: 'src/components/**/[A-Z]*.{ts,tsx}',
+  styleguideDir: '.styleguidist',
+  serverPort: 9000,
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+  },
+};
