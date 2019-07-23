c5d090659291a58b1602948b1c7c2400255f559a
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-07-23 19:34:25 +0300

    update IconButton component

--- src/components/IconButton.md
+++ src/components/IconButton.md
@@ -1,7 +1,29 @@
-```jsx harmony
-import Arrow from 'icons/arrow.svg';
+An Icon button can different types of background colors:
 
-<IconButton>
-  <Arrow />
-</IconButton>;
+```jsx harmony
+import Icon from './Icon';
+
+<React.Fragment>
+  <IconButton variant="primary" mr={2}>
+    <Icon type="user" size="large" />
+  </IconButton>
+  <IconButton variant="default">
+    <Icon type="user" size="large" />
+  </IconButton>
+</React.Fragment>;
+```
+
+It can also be marked as active:
+
+```jsx harmony
+import Icon from './Icon';
+
+<React.Fragment>
+  <IconButton variant="primary" active mr={2}>
+    <Icon type="user" size="large" />
+  </IconButton>
+  <IconButton variant="default" active>
+    <Icon type="user" size="large" />
+  </IconButton>
+</React.Fragment>;
 ```
--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
 import { convertHex } from 'utils/helpers';
-import { Button as RebassButton } from 'rebass';
-import Flex from './Flex';
+import BaseButton, { BaseButtonProps } from './BaseButton';
+
+interface IconButtonProps extends BaseButtonProps {
+  /** Whether the component should appear as "activated". Helpful for menus */
+  active?: boolean;
+
+  /** The color variant of the IconButton */
+  variant?: 'primary' | 'default';
+}
 
 /**
- * A wrapper that makes an Icon be clickable
+ * A wrapper that makes an <a href="/#/Icon">Icon<a> component be clickable
  */
 const IconButton: React.FC = styled(props => (
-  <RebassButton bg="transparent" borderRadius="circle" width={32} height={32} {...props}>
-    <Flex></Flex>
-  </RebassButton>
-))`
-  cursor: pointer;
-  transition: background-color 0.2s linear;
+  <BaseButton bg="transparent" p={3} borderRadius="circle" {...props} />
+))<IconButtonProps>`
+  color: ${({ theme, active, variant }) =>
+    active ? theme.colors[variant === 'primary' ? 'primary300' : 'grey400'] : theme.colors.grey400};
 
-  [disabled] {
-    opacity: 0.3;
-    pointer-events: none;
-  }
+  background-color: ${({ theme, active, variant }) =>
+    active && convertHex(theme.colors[variant === 'primary' ? 'primary300' : 'black'], 0.2)};
 
-  &:hover {
-    background-color: ${({ theme }) => convertHex(theme.colors.black, 0.9)};
+  &:hover:not(:active) {
+    background-color: ${({ theme, variant, active }) =>
+      !active && convertHex(theme.colors[variant === 'primary' ? 'primary300' : 'black'], 0.1)};
   }
 
   &:active {
-    background-color: ${({ theme }) => convertHex(theme.colors.black, 0.8)};
+    background-color: ${({ theme, variant }) =>
+      convertHex(theme.colors[variant === 'primary' ? 'primary300' : 'black'], 0.2)};
   }
 `;
+
+IconButton.defaultProps = {
+  active: false,
+  variant: 'primary',
+};
 
 export default IconButton;
