ff44a3a5dfe1bc6da36dec66926948cf15146cb6
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-07-24 02:21:45 +0300

    add Theme type values

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -1,4 +1,40 @@
-import { Theme } from 'styled-system';
+import { Theme as StyledSystemTheme } from 'styled-system';
+
+type fontWeights = 'normal' | 'bold' | 'bolder';
+type radii = 'none' | 'small' | 'medium' | 'large' | 'circle';
+type fontFamilies = 'primary';
+type colors =
+  | 'transparent'
+  | 'white'
+  | 'grey50'
+  | 'grey100'
+  | 'grey200'
+  | 'grey300'
+  | 'grey400'
+  | 'grey500'
+  | 'black'
+  | 'red100'
+  | 'red200'
+  | 'red300'
+  | 'orange300'
+  | 'yellow300'
+  | 'green100'
+  | 'green200'
+  | 'green300'
+  | 'blue100'
+  | 'blue200'
+  | 'blue300'
+  | 'primary50'
+  | 'primary100'
+  | 'primary200'
+  | 'primary300';
+
+export interface Theme extends StyledSystemTheme {
+  fontWeights: { [key in fontWeights]: number };
+  radii: { [key in radii]: number };
+  fonts: { [key in fontFamilies]: string };
+  colors: { [key in colors]: string };
+}
 
 const theme: Theme = {
   breakpoints: ['1200px'],
