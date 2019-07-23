d9d6c16675252e7781e5f3b4e17c16d0f8478341
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-07-24 00:25:12 +0300

    create Checkbox

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import IconButton from './IconButton';
+import Icon from './Icon';
+
+const StyledCheckbox = styled.input`
+  position: absolute;
+  opacity: 0;
+`;
+
+//<input type="checkbox" name="vehicle3" value="Boat" checked> I have a boat<br>
+
+export interface CheckboxProps {
+  /** Whether the checkbox should be checked or not */
+  checked: boolean;
+
+  /** What happens when the value of the checkbox changes */
+  onChange: (checked: boolean) => void;
+}
+
+const Checkbox: React.FC<CheckboxProps> = ({ checked, onChange, ...rest }) => {
+  return (
+    <IconButton role="checkbox" aria-checked={checked} onClick={() => onChange(!checked)}>
+      <Icon
+        type={checked ? 'checkbox-selected' : 'checkbox'}
+        color={checked ? 'primary300' : 'grey400'}
+      />
+      <StyledCheckbox
+        type="checkbox"
+        checked={checked}
+        onChange={e => onChange(e.target.checked)}
+        {...rest}
+      />
+    </IconButton>
+  );
+};
+
+Checkbox.defaultProps = {
+  checked: false,
+};
+
+export default Checkbox;
