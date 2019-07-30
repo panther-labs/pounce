6d9a7e3f8f82d88549473e092d0618b0a6c6368b
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-07-30 15:07:31 +0300

    Wrap all doc examples with `<Flex>`

--- src/utils/ThemeInjector.tsx
+++ src/utils/ThemeInjector.tsx
@@ -2,13 +2,14 @@ import React, { ReactElement } from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from 'themes/default';
 import GlobalStyles from 'utils/GlobalStyles';
+import Flex from 'components/Flex';
 
 const ThemeInjector: React.FC<{ children: ReactElement }> = ({ children }) => (
   <ThemeProvider theme={theme}>
-    <React.Fragment>
+    <Flex>
       <GlobalStyles />
       {children}
-    </React.Fragment>
+    </Flex>
   </ThemeProvider>
 );
 
