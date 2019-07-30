460b586831dd4978847cab0a37c0e2058094d9ae
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-07-30 15:11:27 +0300

    simplified code of TextInput

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import BaseTextInput, { BaseTextInputProps } from 'components/BaseTextInput';
+import BaseInputElement, { BaseInputElementProps } from 'components/BaseInputElement';
 
-export { BaseTextInputProps as TextInputProps };
+export type TextInputProps = BaseInputElementProps;
 
 /** The typical text input that you are going to use in most forms */
-const TextInput = (props: BaseTextInputProps) => (
-  <BaseTextInput as="input" type="text" {...props} />
-);
+const TextInput = (props: TextInputProps) => <BaseInputElement as="input" {...props} />;
 
 export default TextInput;
