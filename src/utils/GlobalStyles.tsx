602996b32aa859f8f1399c92ea23e0d30ec4d115
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-07-31 11:50:27 +0300

    create DateInput component

--- src/utils/GlobalStyles.tsx
+++ src/utils/GlobalStyles.tsx
@@ -79,6 +79,131 @@ const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.colors.grey200};
   }
 
+/* DayPicker styles */
+
+.DayPicker {
+  display: inline-block;
+  font-size: ${({ theme }) => `${theme.fontSizes[3]}px`};
+}
+
+.DayPicker-Months {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.DayPicker-NavButton {
+  position: absolute;
+  top: ${({ theme }) => `${theme.space[1]}px`};
+
+  display: inline-block;
+  width: ${({ theme }) => `${theme.space[8]}px`};
+  height: ${({ theme }) => `${theme.space[8]}px`};
+  background-position: center;
+  cursor: pointer;
+  
+  &:hover {
+    opacity: 0.8;
+  }
+}
+
+.DayPicker-NavButton--prev {
+  left: ${({ theme }) => `${theme.space[5]}px`};
+  background-image: url('data:image/svg+xml;base64, PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGQ9Ik0xNCA3TDkgMTJMMTQgMTdWN1oiIGZpbGw9IiM0RjRGNEYiLz4NCjwvc3ZnPg==');
+}
+
+.DayPicker-NavButton--next {
+  right: ${({ theme }) => `${theme.space[5]}px`};
+  background-image: url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGQ9Ik0xMCAxN0wxNSAxMkwxMCA3VjE3WiIgZmlsbD0iIzRGNEY0RiIvPg0KPC9zdmc+DQo=');
+}
+
+.DayPicker-NavButton--interactionDisabled {
+  display: none;
+}
+
+.DayPicker-Caption {
+  display: flex;
+  justify-content: center;
+  padding: ${({ theme }) => `${theme.space[3]}px ${theme.space[0]}px`};
+  border-bottom: ${({ theme }) => `1px solid ${theme.colors.grey100}`};
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+}
+
+.DayPicker-Weekdays {
+ display: none;
+}
+
+.DayPicker-Body {
+  display: table-row-group;
+}
+
+.DayPicker-Week {
+  display: table-row;
+}
+
+.DayPicker-Day {
+  display: table-cell;
+  padding: ${({ theme }) => `${theme.space[3]}px`};
+  vertical-align: middle;
+  text-align: center;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.grey400};  
+  border-right: ${({ theme }) => `1px solid ${theme.colors.grey100}`};  
+  border-bottom: ${({ theme }) => `1px solid ${theme.colors.grey100}`};  
+  
+}
+
+.DayPicker--interactionDisabled .DayPicker-Day {
+  cursor: default;
+}
+
+/* Default modifiers */
+
+.DayPicker-Day--today {
+  color: ${({ theme }) => theme.colors.red300};
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+}
+
+.DayPicker-Day--outside {
+  color: ${({ theme }) => theme.colors.grey300};
+  cursor: default;
+}
+
+.DayPicker-Day--disabled {
+  color: #DCE0E0;
+  cursor: default;
+}
+
+.DayPicker-Day--selected:not(.DayPicker-Day--disabled):not(.DayPicker-Day--outside) {
+  background-color: ${({ theme }) => theme.colors.primary300};
+  color: ${({ theme }) => theme.colors.white};
+}
+
+.DayPicker-Day--selected:not(.DayPicker-Day--disabled):not(.DayPicker-Day--outside):hover {
+  background-color: ${({ theme }) => theme.colors.primary300};
+  color: ${({ theme }) => theme.colors.white};
+}
+
+.DayPicker:not(.DayPicker--interactionDisabled)
+  .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
+  background-color: ${({ theme }) => theme.colors.primary50};
+}
+
+/* DayPickerInput */
+
+.DayPickerInput-OverlayWrapper {
+  position: relative;
+}
+
+.DayPickerInput-Overlay {
+  position: absolute;
+  left: 0;
+  z-index: 100;
+
+  background: white;
+  box-shadow: ${({ theme }) => theme.shadows[3]};
+  border-radius: ${({ theme }) => `${theme.radii.small}px`};
+}
 `;
 
 export default GlobalStyles;
