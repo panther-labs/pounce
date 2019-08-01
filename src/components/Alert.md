707da1688d841b467323e14fc9aa6a39a6c30c64
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-08-01 16:03:24 +0300

    support `icon` on Alert

--- src/components/Alert.md
+++ src/components/Alert.md
@@ -1,9 +1,25 @@
+Alerts can be simple:
+
 ```jsx harmony
 import Box from 'components/Box';
 
 <Box width={1} bg="grey50" p={5}>
+  <Alert variant="success" mb={3} content="This is an success alert" />
   <Alert variant="info" mb={3} content="This is an info alert" />
   <Alert variant="warning" mb={3} content="This is a warning alert" />
   <Alert variant="error" content="This is an error alert" />
+</Box>;
+```
+
+Alerts can also contain icons:
+
+```jsx harmony
+import Box from 'components/Box';
+
+<Box width={1} bg="grey50" p={5}>
+  <Alert icon="check" variant="success" mb={3} content="This is an success alert" />
+  <Alert icon="edit" variant="info" mb={3} content="This is an info alert" />
+  <Alert icon="warning" variant="warning" mb={3} content="This is a warning alert" />
+  <Alert icon="delete" variant="error" content="This is an error alert" />
 </Box>;
 ```
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import Text from 'components/Text';
 import Card from 'components/Card';
+import Flex from 'components/Flex';
+import Icon, { svgComponentMapping } from 'components/Icon';
 
 export interface AlertProps {
   /** The style of the Alert */
-  variant: 'info' | 'warning' | 'error';
+  variant: 'success' | 'info' | 'warning' | 'error';
 
   /** The content (html or text) of the the component */
   content: React.ReactNode;
+
+  /** The content (html or text) of the the component */
+  icon?: keyof typeof svgComponentMapping;
 }
 
 /** An Alert component is simply a container for text that should capture the user's attention */
-const Alert: React.FC<AlertProps> = ({ content, variant, ...rest }) => {
+const Alert: React.FC<AlertProps> = ({ content, variant, icon, ...rest }) => {
   const variantProps = (function() {
     switch (variant) {
+      case 'success':
+        return { borderColor: 'green300', color: 'grey400' };
       case 'info':
         return { borderColor: 'blue300', color: 'grey400' };
       case 'warning':
@@ -24,11 +31,22 @@ const Alert: React.FC<AlertProps> = ({ content, variant, ...rest }) => {
     }
   })();
 
+  const textNode = (
+    <Text size="large" as="p" color={variantProps.color}>
+      {content}
+    </Text>
+  );
+
   return (
     <Card py={4} px={8} borderLeft="3px solid" borderColor={variantProps.borderColor} {...rest}>
-      <Text size="large" as="p" color={variantProps.color}>
-        {content}
-      </Text>
+      {icon ? (
+        <Flex alignItems="center">
+          <Icon type={icon} mr={4} color={variantProps.borderColor} />
+          {textNode}
+        </Flex>
+      ) : (
+        textNode
+      )}
     </Card>
   );
 };
