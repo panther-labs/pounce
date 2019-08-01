cd21b0f1cd399f5acb01f45cf60346506c932b06
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-08-01 19:10:18 +0300

    Alert can now be discardable by the user

--- src/components/Alert.md
+++ src/components/Alert.md
@@ -11,7 +11,7 @@ import Box from 'components/Box';
 </Box>;
 ```
 
-Alerts can also contain icons:
+Alerts can contain icons:
 
 ```jsx harmony
 import Box from 'components/Box';
@@ -24,7 +24,7 @@ import Box from 'components/Box';
 </Box>;
 ```
 
-Alerts can also have descriptions:
+Alerts can have descriptions:
 
 ```jsx harmony
 import Box from 'components/Box';
@@ -56,6 +56,46 @@ import Box from 'components/Box';
     variant="error"
     title="This is an error alert"
     description="This is a nice description of the title"
+  />
+</Box>;
+```
+
+Alerts can be hidden by the user:
+
+```jsx harmony
+import Box from 'components/Box';
+
+<Box width={1} bg="grey50" p={5}>
+  <Alert
+    icon="check"
+    variant="success"
+    mb={3}
+    title="This is an success alert"
+    description="This is a nice description of the title"
+    discardable
+  />
+  <Alert
+    icon="edit"
+    variant="info"
+    mb={3}
+    title="This is an info alert"
+    description="This is a nice description of the title"
+    discardable
+  />
+  <Alert
+    icon="warning"
+    variant="warning"
+    mb={3}
+    title="This is a warning alert"
+    description="This is a nice description of the title"
+    discardable
+  />
+  <Alert
+    icon="delete"
+    variant="error"
+    title="This is an error alert"
+    description="This is a nice description of the title"
+    discardable
   />
 </Box>;
 ```
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Text from 'components/Text';
 import Card from 'components/Card';
-import Box from 'components/Box';
+import Box, { BoxProps } from 'components/Box';
 import Flex from 'components/Flex';
+import IconButton from 'components/IconButton';
 import Icon, { svgComponentMapping } from 'components/Icon';
 
-export interface AlertProps {
+export interface AlertProps extends Omit<BoxProps, 'title'> {
   /** The style of the Alert */
   variant: 'success' | 'info' | 'warning' | 'error';
 
@@ -17,10 +18,22 @@ export interface AlertProps {
 
   /** The type of the icon that will accompany this alert */
   icon?: keyof typeof svgComponentMapping;
+
+  /** Whether the Alert should have a close button in order to remove itself */
+  discardable?: boolean;
 }
 
 /** An Alert component is simply a container for text that should capture the user's attention */
-const Alert: React.FC<AlertProps> = ({ title, description, variant, icon, ...rest }) => {
+const Alert: React.FC<AlertProps> = ({
+  title,
+  description,
+  variant,
+  icon,
+  discardable,
+  ...rest
+}) => {
+  const [open, setOpen] = React.useState(true);
+
   const variantProps = (function() {
     switch (variant) {
       case 'success':
@@ -51,8 +64,15 @@ const Alert: React.FC<AlertProps> = ({ title, description, variant, icon, ...res
     </Box>
   );
 
-  return (
-    <Card py={4} px={7} borderLeft="3px solid" borderColor={variantProps.borderColor} {...rest}>
+  return open ? (
+    <Card
+      position="relative"
+      py={4}
+      px={7}
+      borderLeft="3px solid"
+      borderColor={variantProps.borderColor}
+      {...rest}
+    >
       {icon ? (
         <Flex alignItems="flex-start">
           <Icon type={icon} mr={4} color={variantProps.borderColor} />
@@ -61,13 +81,21 @@ const Alert: React.FC<AlertProps> = ({ title, description, variant, icon, ...res
       ) : (
         textNode
       )}
+      {discardable && (
+        <Flex position="absolute" right={3} top={0} bottom={0} alignItems="center">
+          <IconButton onClick={() => setOpen(false)}>
+            <Icon type="close" size="large" color="grey200" />
+          </IconButton>
+        </Flex>
+      )}
     </Card>
-  );
+  ) : null;
 };
 
 Alert.defaultProps = {
   icon: undefined,
   description: undefined,
+  discardable: false,
 };
 
 export default Alert;
