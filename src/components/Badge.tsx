7b82d3c1ee6698ffb1a26fd07ca7da469b9219b2
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-08-02 13:35:17 +0300

    implement Pill

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,46 +1,19 @@
 import React from 'react';
-import Text from 'components/Text';
+import BaseBadge, { BaseBadgeProps } from 'components/BaseBadge';
 
-export interface BadgeProps {
-  /** The style of the badge */
-  variant: 'info' | 'error-low' | 'error-medium' | 'error-high' | 'error-critical';
-}
+export type BadgeProps = BaseBadgeProps;
 
-/** A badge is simply a visual label to associate with something else */
-const Badge: React.FC<BadgeProps> = ({ variant, ...rest }) => {
-  const variantProps = (function() {
-    switch (variant) {
-      case 'info':
-        return {
-          border: '1px solid',
-          borderColor: 'grey200',
-          bg: 'transparent',
-          color: 'grey300',
-          children: 'INFO',
-        };
-      case 'error-low':
-        return { bg: 'grey100', color: 'grey300', children: 'LOW' };
-      case 'error-medium':
-        return { bg: 'blue100', color: 'blue300', children: 'MEDIUM' };
-      case 'error-high':
-        return { bg: 'red100', color: 'red300', children: 'HIGH' };
-      case 'error-critical':
-      default:
-        return { bg: 'red300', color: 'white', children: 'CRITICAL' };
-    }
-  })();
-
+/** A badge is simply a visual label to accompany & characterize a certain text*/
+const Badge: React.FC<BadgeProps> = props => {
   return (
-    <Text
-      width={62}
+    <BaseBadge
+      width="62px"
       textAlign="center"
-      fontSize={0}
       fontWeight="bold"
       borderRadius="small"
       px={3}
       py={1}
-      {...variantProps}
-      {...rest}
+      {...props}
     />
   );
 };
