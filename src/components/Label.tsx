e9ab321c3ed29165916fb237cf6fb815c935b912
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2019-08-05 18:28:32 +0300

    Feat Pill & proper line-height (#14)
    Add proper line height

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -15,14 +15,14 @@ const Label: React.FC<LabelProps> = ({ size, ...rest }) => {
   const sizeProps = (function() {
     switch (size) {
       case 'large':
-        return { fontWeight: 'bold', fontSize: 3 };
+        return { fontWeight: 'bold', fontSize: 3, lineHeight: 3 };
       case 'medium':
-        return { fontWeight: 'bold', fontSize: 2 };
+        return { fontWeight: 'bold', fontSize: 2, lineHeight: 2 };
       case 'small':
-        return { fontWeight: 'bolder', fontSize: 1 };
+        return { fontWeight: 'bolder', fontSize: 1, lineHeight: 1 };
       case 'extra-small':
       default:
-        return { fontWeight: 'bold', fontSize: 0 };
+        return { fontWeight: 'bold', fontSize: 0, lineHeight: 0 };
     }
   })() as { fontWeight: keyof DefaultTheme['fontWeights']; fontSize: number };
 
