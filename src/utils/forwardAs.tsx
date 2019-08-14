071040465978fc8e5ba7a8ea951cb411cd1b9330
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-08-14 14:56:11 +0300

    fix a bug with `forwardAs

--- src/utils/forwardAs.tsx
+++ src/utils/forwardAs.tsx
@@ -5,11 +5,11 @@ import { BoxProps } from 'components/Box';
  * A HOC that makes sure that styled components will not immediately consume the `as` prop, but
  * push it down the component chain
  */
-const forwardAs = <P extends BoxProps>(
-  Component: React.FC<P & { forwardedAs: BoxProps['as'] }>
-) => {
-  // @ts-ignore
-  const ForwardAs: React.FC<P> = ({ as, ...rest }) => <Component forwardedAs={as} {...rest} />;
+const forwardAs = <P extends BoxProps>(Component: React.FC<P>) => {
+  const ForwardAs: React.FC<P> = ({ as, ...rest }) =>
+    // @ts-ignore
+    as ? <Component forwardedAs={as} {...rest} /> : <Component {...rest} />;
+
   return ForwardAs;
 };
 
