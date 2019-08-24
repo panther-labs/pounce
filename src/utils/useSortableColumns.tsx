9d927843e11a341b7fc0721d1fec4035e1efc7d2
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-08-24 13:47:08 +0300

    Revert "replace `null` with `undefined`"
    This reverts commit 72095c1fe52a083e93b66121f7314474e8bba2b6.

--- src/utils/useSortableColumns.tsx
+++ src/utils/useSortableColumns.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 
 export interface UseSortableTableRows {
   /** The initial value of the sort direction */
-  initialSortDir?: 'ascending' | 'descending';
+  initialSortDir?: 'ascending' | 'descending' | null;
 
   /**
    * The initial value of the sort key. The value must match the `keys` of one of the columns or be
    * null if the data is not currently being sorted under a certain column
    */
-  initialSortKey?: string;
+  initialSortKey?: string | null;
 }
 
 /**
  * A hook that adds the ability for columns to be sortable and handles state management for them
  * */
-function useSortableTableRows({ initialSortDir, initialSortKey }: UseSortableTableRows) {
+function useSortableTableRows({
+  initialSortDir = null,
+  initialSortKey = null,
+}: UseSortableTableRows) {
   const [sortDir, setSortDir] = React.useState(initialSortDir);
   const [sortKey, setSortKey] = React.useState(initialSortKey);
 
