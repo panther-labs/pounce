fd960d98567ea7fdd0a593d3de52183c8ebccab3
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-08-26 14:36:12 +0300

    add `total` as a parameter in table hook

--- src/utils/usePaginatedTable.tsx
+++ src/utils/usePaginatedTable.tsx
@@ -9,13 +9,6 @@ import MenuItem from 'components/MenuItem';
 
 export interface UsePaginatedTable {
   /**
-   * The total number of items that this table will show. This prop is only used to display the
-   * total number of pages that exist for a selected page size. You can omit it if you don't want
-   * the total number of pages to be shown
-   */
-  total?: number;
-
-  /**
    * A list of page sizes that the user is going to be able to select from
    * */
   pageSizes?: number[];
@@ -37,7 +30,6 @@ export interface UsePaginatedTable {
  * serial number of each row
  * */
 const usePaginatedTable = ({
-  total = 0,
   pageSizes = [25, 50, 75, 100],
   initialPageSizeIndex = 0,
   initialPageIndex = 0,
@@ -46,66 +38,69 @@ const usePaginatedTable = ({
   const [activePageSizeIndex, setActivePageSizeIndex] = React.useState(initialPageSizeIndex);
 
   const itemsPerPage = pageSizes[activePageSizeIndex];
-  const totalPages = Math.ceil(total / itemsPerPage);
 
   return React.useMemo(
     () => ({
       startIndex: itemsPerPage * activePageIndex,
       endIndex: itemsPerPage * (activePageIndex + 1) - 1,
       itemsPerPage,
-      totalPages,
       activePageIndex,
-      paginationElement: (
-        <Flex alignItems="center" justifyContent="center">
-          <Flex mr={9} alignItems="center">
-            <IconButton
-              variant="default"
-              disabled={activePageIndex <= 0}
-              onClick={() => setActivePageIndex(activePageIndex - 1)}
-            >
-              <Icon size="large" type="chevron-left" />
-            </IconButton>
-            {total > 0 && (
-              <Label size="large" mx={4} color="grey400">
-                {activePageIndex + 1} of {totalPages}
-              </Label>
-            )}
-            <IconButton
-              variant="default"
-              disabled={activePageIndex >= totalPages - 1}
-              onClick={() => setActivePageIndex(activePageIndex + 1)}
-            >
-              <Icon size="large" type="chevron-right" />
-            </IconButton>
-          </Flex>
-          {pageSizes.length > 1 && (
-            <Flex alignItems="center">
-              <Dropdown
-                trigger={
-                  <Flex alignItems="center">
-                    <Text size="medium" mr={2}>
-                      {itemsPerPage} per page
-                    </Text>
-                    <Icon size="small" type="rules" />
-                  </Flex>
-                }
+
+      // eslint-disable-next-line react/display-name
+      paginationElement: (total: number) => {
+        const totalPages = Math.ceil(total / itemsPerPage);
+        return (
+          <Flex alignItems="center" justifyContent="center">
+            <Flex mr={9} alignItems="center">
+              <IconButton
+                variant="default"
+                disabled={activePageIndex <= 0}
+                onClick={() => setActivePageIndex(activePageIndex - 1)}
               >
-                {pageSizes.map((pageSize, index) => (
-                  <Dropdown.Item
-                    key={pageSize}
-                    onSelect={() => {
-                      setActivePageSizeIndex(index);
-                      setActivePageIndex(0);
-                    }}
-                  >
-                    <MenuItem variant="default">{pageSize} per page</MenuItem>
-                  </Dropdown.Item>
-                ))}
-              </Dropdown>
+                <Icon size="large" type="chevron-left" />
+              </IconButton>
+              {total > 0 && (
+                <Label size="large" mx={4} color="grey400">
+                  {activePageIndex + 1} of {Math.ceil(total / itemsPerPage)}
+                </Label>
+              )}
+              <IconButton
+                variant="default"
+                disabled={activePageIndex >= totalPages - 1}
+                onClick={() => setActivePageIndex(activePageIndex + 1)}
+              >
+                <Icon size="large" type="chevron-right" />
+              </IconButton>
             </Flex>
-          )}
-        </Flex>
-      ),
+            {pageSizes.length > 1 && (
+              <Flex alignItems="center">
+                <Dropdown
+                  trigger={
+                    <Flex alignItems="center">
+                      <Text size="medium" mr={2}>
+                        {itemsPerPage} per page
+                      </Text>
+                      <Icon size="small" type="rules" />
+                    </Flex>
+                  }
+                >
+                  {pageSizes.map((pageSize, index) => (
+                    <Dropdown.Item
+                      key={pageSize}
+                      onSelect={() => {
+                        setActivePageSizeIndex(index);
+                        setActivePageIndex(0);
+                      }}
+                    >
+                      <MenuItem variant="default">{pageSize} per page</MenuItem>
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown>
+              </Flex>
+            )}
+          </Flex>
+        );
+      },
     }),
     [itemsPerPage, activePageIndex]
   );
