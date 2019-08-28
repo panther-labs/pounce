5b2678308b0ed14924b193020998f71e2801fae6
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2019-08-28 09:59:57 +0300

    remove hardcoded width from Badge

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -6,15 +6,7 @@ export type BadgeProps = BaseBadgeProps;
 /** A badge is simply a visual label to accompany & characterize a certain text*/
 const Badge: React.FC<BadgeProps> = props => {
   return (
-    <BaseBadge
-      width="62px"
-      textAlign="center"
-      fontWeight="bold"
-      borderRadius="small"
-      px={3}
-      py={1}
-      {...props}
-    />
+    <BaseBadge textAlign="center" fontWeight="bold" borderRadius="small" px={3} py={1} {...props} />
   );
 };
 
