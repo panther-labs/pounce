fe36b9743821e15b06f6b9cff6465c4c037747cd
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-04-05 21:42:57 +0300

    refactor(flex): minor refactor in prop passing

--- src/components/Flex/Flex.tsx
+++ src/components/Flex/Flex.tsx
@@ -19,14 +19,14 @@ export type FlexProps = Omit<BoxProps, 'display'> & {
  * wrapper around a certain layout
  */
 const Flex: React.FC<FlexProps> = React.forwardRef(function Flex(props, ref) {
-  const { direction, justify, align, flexDirection, justifyContent, alignItems, ...rest } = props;
+  const { direction, justify, align, ...rest } = props;
   return (
     <Box
       display="flex"
       ref={ref}
-      flexDirection={direction ?? flexDirection}
-      justifyContent={justify ?? justifyContent}
-      alignItems={align ?? alignItems}
+      flexDirection={direction}
+      justifyContent={justify}
+      alignItems={align}
       {...rest}
     />
   );
