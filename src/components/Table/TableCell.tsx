d7b1d8a38b5b3425d34ff6df1f07bcde791d20c5
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-04-23 14:00:57 +0300

    feat: simplify TableCell `wrapText` prop

--- src/components/Table/TableCell.tsx
+++ src/components/Table/TableCell.tsx
@@ -2,20 +2,17 @@ import React from 'react';
 import Box, { BoxProps, ReactAttributes } from '../Box';
 import { useTable } from './Table';
 
-export type TableCellProps = Pick<
-  BoxProps,
-  'truncated' | 'color' | 'width' | 'maxWidth' | 'whiteSpace'
-> &
+export type TableCellProps = Pick<BoxProps, 'truncated' | 'color' | 'width' | 'maxWidth'> &
   ReactAttributes<React.AllHTMLAttributes<HTMLTableCellElement>, HTMLTableCellElement> & {
-    /** Whether the content should wrap to multiple lines. Defaults to `true` */
-    wrapText?: boolean;
+    /** Whether the content should wrap to multiple lines. Defaults to `auto` */
+    wrapText?: 'wrap' | 'nowrap' | 'auto';
 
     /** The alignment of the cell. Defaults to `left` */
     align?: BoxProps['textAlign'];
   };
 
 const TableCell: React.FC<TableCellProps> = React.forwardRef(function TableCell(
-  { wrapText = true, align = 'left', ...rest },
+  { wrapText = 'auto', align = 'left', ...rest },
   ref
 ) {
   const { size } = useTable();
@@ -29,7 +26,8 @@ const TableCell: React.FC<TableCellProps> = React.forwardRef(function TableCell(
       color="black"
       fontSize={2}
       textAlign={align}
-      wordBreak={wrapText ? 'break-word' : undefined}
+      wordBreak={wrapText === 'wrap' ? 'break-word' : undefined}
+      whiteSpace={wrapText === 'nowrap' ? 'nowrap' : undefined}
       verticalAlign="middle"
       {...rest}
     />
