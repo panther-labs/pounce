2ec0fc4586ac7bfb2e796903c2a0e80e4b126b08
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-05-20 14:48:02 +0300

    fix: add missing typing

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -5,7 +5,7 @@ import typography from './typography';
 import icons from './icons';
 
 type radii = 'none' | 'small' | 'medium' | 'large' | 'circle';
-type fontFamilies = 'primary';
+type fontFamilies = 'primary' | 'mono';
 type shadows = 'none' | 'dark50' | 'dark100' | 'dark150' | 'dark200' | 'dark250';
 
 export interface Theme extends StyledSystemTheme {
