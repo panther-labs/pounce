e5757969c1ed13962db5d19aa0bafd217c3f3f67
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-05-26 18:29:02 +0300

    feat: add FormError element

---
+++ src/components/FormError/FormError.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Icon from '../Icon';
+import Flex, { FlexProps } from '../Flex';
+
+type FormErrorProps = FlexProps;
+
+/**
+ * Extends Box
+ *
+ * A utility component that quickly allows you to add error messages to form fields
+ */
+const FormError: React.FC<FormErrorProps> = ({ children, ...rest }) => {
+  return (
+    <Flex
+      as="p"
+      alignItems="center"
+      color="red-200"
+      fontSize="small"
+      fontStyle="italic"
+      role="alert"
+      {...rest}
+    >
+      <Icon size="small" type="warning" mr={1} flex="0 0 auto" />
+      {children}
+    </Flex>
+  );
+};
+
+export default FormError;
