95d86585a904ec024c1f51d9a24f4f4ba9ee12f7
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-05-27 16:19:58 +0300

    feat: refactor MultiCombobox to latest designs

--- src/components/MultiCombobox/MultiCombobox.tsx
+++ src/components/MultiCombobox/MultiCombobox.tsx
@@ -2,30 +2,19 @@
 import React from 'react';
 import Downshift, { DownshiftState, StateChangeOptions } from 'downshift';
 import { filter as fuzzySearch } from 'fuzzaldrin';
-import Card from '../Card';
-import Box, { BoxProps } from '../Box';
+import Box from '../Box';
 import MenuItem from '../MenuItem';
-import { TextInputProps } from '../TextInput';
-import IconButton from '../IconButton';
 import Icon from '../Icon';
-import Chip from '../Chip';
 import Flex from '../Flex';
-import { InputElementOuterBox, InputElementLabel, InputElementInnerBox } from '../BaseInputElement';
+import { InputControl, InputLabel, InputElement, InputElementProps } from '../utils/Input';
+import Tag from './Tag';
 
-export interface RenderMultiComboboxItemProps<T> {
-  /** The item to render */
-  item: T;
-
-  /** Whether this item is selected **/
-  selected: boolean;
-
-  /** Whether this item is simply highlighted using the keyboard. This doesn't mean it's selected */
-  highlighted: boolean;
-}
-
-export interface MultiComboboxProps<T> {
+export type MultiComboboxProps<T> = {
   /** Callback when the selection changes */
   onChange: (value: T[]) => void;
+
+  /** The label associated with this dropdown form element */
+  label: string;
 
   /** A list of entries that the dropdown will have as options */
   items: T[];
@@ -38,15 +27,10 @@ export interface MultiComboboxProps<T> {
   itemToString?: (item: T) => string;
 
   /**
-   * An optional function to override the default way that each item renders. In a
-   * MultiSelectMultiCombobox, the `selected` is always going to be `false`, since the selected values
-   * are not visible in the menu
-   * */
-  renderItem?: ({
-    item,
-    selected,
-    highlighted,
-  }: RenderMultiComboboxItemProps<T>) => React.ReactElement;
+   * A function that accepts an item as a parameteer and returns `true` if the item should be
+   * disabled or `false` otherwise. Defaults to `() => false`.
+   */
+  disableItem?: (item: T) => boolean;
 
   /**
    * The value of the item that is currently selected. The component is a controlled one,
@@ -54,17 +38,20 @@ export interface MultiComboboxProps<T> {
    * */
   value: T[];
 
-  /** The label associated with this dropdown form element */
-  label?: string;
+  /** Whether the multi-combobox has an invalid value */
+  invalid?: boolean;
+
+  /** Whether the multi-combobox is required or not */
+  required?: boolean;
+
+  /** Whether the multi-combobox is disabled or not */
+  disabled?: boolean;
+
+  /** A placeholder that's visible when the user focuses on the Combobox */
+  placeholder?: string;
 
   /** Whether the MultiCombobox should have an input to search results */
   searchable?: boolean;
-
-  /** Whether the component should be disabled or not */
-  disabled?: boolean;
-
-  /** A set of props & attributes that will be given to the input */
-  inputProps?: TextInputProps;
 
   /**
    * Allow the user to add custom entries to the dropdown instead of limiting selections to the
@@ -87,13 +74,7 @@ export interface MultiComboboxProps<T> {
    * item will be added to the selection. If not, then this item won't be added
    * */
   validateAddition?: (userEnteredInput: string) => boolean;
-
-  /** A set of props & attributes to apply to the wrapping `Box` of the entire module */
-  rootProps?: BoxProps;
-
-  /** A set of props & attributes to apply to the container of the items (the Menu) */
-  menuProps?: BoxProps;
-}
+};
 
 const stateReducer = (state: DownshiftState<any>, changes: StateChangeOptions<any>) => {
   switch (changes.type) {
@@ -122,23 +103,23 @@ const stateReducer = (state: DownshiftState<any>, changes: StateChangeOptions<an
  * A simple MultiCombobox can be thought of as a typical `<select>` component. Whenerever you would
  * use a normal select, you should now pass the `<MultiCombobox>` component.
  */
-function MultiCombobox<ItemShape>({
+function MultiCombobox<Item extends string | object>({
   onChange,
   value,
   items,
-  renderItem,
+  disableItem = () => false,
   searchable = false,
   label = '',
-  inputProps = {},
-  rootProps: userRootProps = {},
-  menuProps: userMenuProps = {},
   disabled = false,
+  placeholder = '',
   itemToString = item => String(item),
   allowAdditions = false,
   validateAddition = () => true,
   maxHeight = 300,
   maxResults,
-}: MultiComboboxProps<ItemShape>): React.ReactElement<MultiComboboxProps<ItemShape>> {
+  invalid,
+  ...rest
+}: MultiComboboxProps<Item>): React.ReactElement<MultiComboboxProps<Item>> {
   const removeItem = (item: any) => {
     onChange(value.filter(i => i !== item));
   };
@@ -150,159 +131,173 @@ function MultiCombobox<ItemShape>({
   };
 
   return (
-    <Box position="relative">
-      <Downshift
-        stateReducer={stateReducer}
-        onChange={addSelectedItem}
-        selectedItem={null}
-        itemToString={item => (item ? itemToString(item) : '')}
-        initialInputValue=""
-      >
-        {({
-          getRootProps,
-          getInputProps,
-          getItemProps,
-          getMenuProps,
-          highlightedIndex,
-          inputValue,
-          isOpen,
-          toggleMenu,
-          openMenu,
-          getLabelProps,
-          selectItem,
-        }) => {
-          // If it's a multicombobox we DON'T WANT t oinclude the results already selected and also
-          // we want to make sure that the results get filtered by the search term of the user
-          const nonSelectedItems = items.filter(
-            item => !value.map(itemToString).includes(itemToString(item))
-          );
+    <Downshift<Item>
+      stateReducer={stateReducer}
+      onChange={addSelectedItem}
+      selectedItem={null}
+      itemToString={item => (item ? itemToString(item) : '')}
+      initialInputValue=""
+    >
+      {({
+        getRootProps,
+        getInputProps,
+        getItemProps,
+        getMenuProps,
+        getLabelProps,
+        highlightedIndex,
+        inputValue,
+        isOpen,
+        toggleMenu,
+        openMenu,
+        selectedItem,
+        selectItem,
+      }) => {
+        // If it's a multicombobox we DON'T WANT t oinclude the results already selected and also
+        // we want to make sure that the results get filtered by the search term of the user
+        const nonSelectedItems = items.filter(
+          item => !value.map(itemToString).includes(itemToString(item))
+        );
 
-          // From the non-selected items, make sure to filter the ones that match the user's
-          // search term. To do that we convert our items to their string representations
-          const strResults = fuzzySearch(nonSelectedItems.map(itemToString), inputValue || '');
+        // From the non-selected items, make sure to filter the ones that match the user's
+        // search term. To do that we convert our items to their string representations
+        const strResults = fuzzySearch(nonSelectedItems.map(itemToString), inputValue || '');
 
-          // and then convert those strings back to the original shape of the items, while making
-          // sure to only display a (potentially) limited number of them
-          const results = items
-            .filter(item => strResults.includes(itemToString(item)))
-            .slice(0, maxResults);
+        // and then convert those strings back to the original shape of the items, while making
+        // sure to only display a (potentially) limited number of them
+        const results = items
+          .filter(item => strResults.includes(itemToString(item)))
+          .slice(0, maxResults);
 
-          // Only show the items that have not been selected
+        // Only show the items that have not been selected
 
-          // We add 3 types of additional data to the input that is going to be renders:
-          // 1. `inputProps` that the user asked us to add
-          // 2. A handler for the `Delete` button, so that you can delete tokens with a single key
-          // 3. When the combobox is not searchable, we make the input "behave" like a div. We
-          // still want an input though for placeholder, spacings, etc.
-          const additionalInputProps = {
-            ...inputProps,
-            ...(!searchable && {
-              style: { cursor: 'pointer' },
-              onMouseDown: toggleMenu,
-              readOnly: true,
-              'aria-readonly': true,
-            }),
-            onFocus: openMenu,
-            onKeyDown: (event: React.KeyboardEvent) => {
-              // Allow deletions of selections by pressing backspace
-              if (event.key === 'Backspace' && !inputValue) {
-                removeItem(value[value.length - 1]);
+        // We add 2 types of additional data to the input that is going to be renders:
+        // 1. A handler for the `Delete` button, so that you can delete tokens with a single key
+        // 3. When the combobox is not searchable, we make the input "behave" like a div. We
+        // still want an input though for placeholder, spacings, etc.
+        const additionalInputProps = {
+          ...rest,
+          ...(!searchable && {
+            cursor: 'pointer',
+            onMouseDown: toggleMenu,
+            readOnly: true,
+            placeholder: !value.length ? placeholder : '',
+            position: !value.length ? 'static' : 'absolute',
+          }),
+          ...(searchable && {
+            placeholder: isOpen || (value.length && allowAdditions) ? placeholder : '',
+            mt: (isOpen && value.length) || (isOpen && value.length && allowAdditions) ? -4 : 0,
+            position: isOpen || (isOpen && value.length && allowAdditions) ? 'static' : 'absolute',
+          }),
+          width: '100%',
+          height: '100%',
+          top: 0,
+          left: 0,
+          onFocus: openMenu,
+          onKeyDown: (event: React.KeyboardEvent) => {
+            // Allow deletions of selections by pressing backspace
+            if (event.key === 'Backspace' && !inputValue) {
+              removeItem(value[value.length - 1]);
+            }
+
+            // Allow the user to add custom selections if both `searchable` and `allowAdditions`
+            // have a truthy value
+            if (event.key === 'Enter' && searchable && allowAdditions && inputValue) {
+              event.preventDefault();
+
+              // By default validateAddition always returns true. Can be overriden by the user
+              // for fine-grained addition
+              if (validateAddition && validateAddition(inputValue)) {
+                selectItem(inputValue as Item, { inputValue: '', isOpen: true });
               }
+            }
+          },
+        };
 
-              // Allow the user to add custom selections if both `searchable` and `allowAdditions`
-              // have a truthy value
-              if (event.key === 'Enter' && searchable && allowAdditions && inputValue) {
-                event.preventDefault();
-
-                // By default validateAddition always returns true. Can be overriden by the user
-                // for fine-grained addition
-                if (validateAddition && validateAddition(inputValue)) {
-                  selectItem(inputValue, { inputValue: '' });
-                }
-              }
-            },
-          };
-
-          const { innerRootRef, ...downshiftRootProps } = getRootProps(
-            { refKey: 'innerRootRef' },
-            { suppressRefError: true }
-          );
-          const { innerMenuRef, ...downshiftMenuProps } = getMenuProps(
-            { refKey: 'innerMenuRef' },
-            { suppressRefError: true }
-          );
-
-          return (
-            <Box {...downshiftRootProps} {...userRootProps} ref={innerRootRef}>
-              {!!label && <InputElementLabel {...getLabelProps()}>{label}</InputElementLabel>}
-              <InputElementOuterBox position="relative" pr={10} disabled={disabled}>
-                <Flex alignItems="center" flexWrap="wrap">
-                  {value.map(selectedItem => (
-                    <Chip
-                      key={itemToString(selectedItem)}
-                      content={itemToString(selectedItem)}
-                      onClick={() => removeItem(selectedItem)}
-                      m={1}
-                    />
-                  ))}
-                  <InputElementInnerBox
-                    {...getInputProps(additionalInputProps)}
-                    as="input"
-                    type="text"
-                    flex="1 0 auto"
-                  />
-                  {items.length > 0 && (
-                    <IconButton
-                      type="button"
-                      variant="default"
-                      position="absolute"
-                      right={3}
-                      onClick={() => toggleMenu()}
-                      tabIndex={-1}
-                      p={2}
-                    >
-                      <Icon size="small" type={isOpen ? 'caret-up' : 'caret-down'} />
-                    </IconButton>
-                  )}
-                </Flex>
-              </InputElementOuterBox>
-              <Box {...downshiftMenuProps} {...userMenuProps} ref={innerMenuRef}>
-                {isOpen && (
-                  <Card
-                    zIndex={1}
-                    mt={2}
-                    position="absolute"
-                    width={1}
-                    maxHeight={maxHeight}
-                    style={{ overflow: 'auto' }}
-                  >
-                    {results.map((item, index) => (
-                      <Box {...getItemProps({ item })} key={itemToString(item)}>
-                        {renderItem ? (
-                          renderItem({
-                            item,
-                            selected: false,
-                            highlighted: highlightedIndex === index,
-                          })
-                        ) : (
-                          <MenuItem
-                            variant="default"
-                            highlighted={highlightedIndex === index}
-                            selected={false}
-                          >
-                            {itemToString(item)}
-                          </MenuItem>
-                        )}
-                      </Box>
+        return (
+          <Box position="relative" {...getRootProps()}>
+            <Box position="relative">
+              <InputControl
+                invalid={invalid}
+                disabled={disabled}
+                variant={isOpen && items.length ? 'filled' : 'outlined'}
+              >
+                {value.length > 0 && (
+                  <Flex as="ul" wrap="wrap" pl={3} pr={10} pt="19px" pb="2px" mx={-1}>
+                    {value.map(selectedItem => (
+                      <Tag
+                        as="li"
+                        key={itemToString(selectedItem)}
+                        m={1}
+                        onRemove={() => removeItem(selectedItem)}
+                      >
+                        {itemToString(selectedItem)}
+                      </Tag>
                     ))}
-                  </Card>
+                  </Flex>
                 )}
-              </Box>
+                <InputElement
+                  as="input"
+                  type="text"
+                  {...(getInputProps(additionalInputProps) as InputElementProps)}
+                />
+                <InputLabel raised={!!value.length} {...getLabelProps()}>
+                  {label}
+                </InputLabel>
+              </InputControl>
+
+              {items.length > 0 && (
+                <Icon
+                  type={isOpen ? 'caret-up' : 'caret-down'}
+                  position="absolute"
+                  pointerEvents="none"
+                  my="auto"
+                  top={0}
+                  bottom={0}
+                  right={4}
+                  color="gray-50"
+                />
+              )}
             </Box>
-          );
-        }}
-      </Downshift>
-    </Box>
+            <Box as="ul" mt="-2px" {...getMenuProps()}>
+              {isOpen && (
+                <Box
+                  as="li"
+                  border="1px solid"
+                  borderLeftColor="blue-600"
+                  borderRightColor="blue-600"
+                  borderBottomColor="blue-600"
+                  borderTopColor="navyblue-600"
+                  borderBottomLeftRadius="medium"
+                  borderBottomRightRadius="medium"
+                  backgroundColor="navyblue-450"
+                  zIndex={1}
+                  position="absolute"
+                  width={1}
+                  maxHeight={maxHeight}
+                  overflow="auto"
+                >
+                  {results.map((item, index) => (
+                    <Box
+                      {...getItemProps({ item, disabled: disableItem(item) })}
+                      key={itemToString(item)}
+                      cursor="pointer"
+                    >
+                      <MenuItem
+                        highlighted={highlightedIndex === index}
+                        selected={item === selectedItem}
+                        disabled={disableItem(item)}
+                      >
+                        {itemToString(item)}
+                      </MenuItem>
+                    </Box>
+                  ))}
+                </Box>
+              )}
+            </Box>
+          </Box>
+        );
+      }}
+    </Downshift>
   );
 }
 
