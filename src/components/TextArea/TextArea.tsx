29ff03cc3a4b73904de5180624d20b3c0f49ba21
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-05-27 15:58:46 +0300

    refactor: textarea and input with utils Input

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import { ReactAttributes } from '../Box';
-import AbstractInput, { AbstractInputProps } from '../AbstractInput';
+import { InputControl, InputElement, InputLabel } from '../utils/Input';
+import { slugify } from '../../utils/helpers';
 
-export type TextAreaProps = ReactAttributes<React.TextareaHTMLAttributes<HTMLTextAreaElement>> &
-  Pick<AbstractInputProps, 'variant' | 'label' | 'invalid'>;
+export type TextAreaProps = ReactAttributes<React.TextareaHTMLAttributes<HTMLTextAreaElement>> & {
+  /** The label that is associated with this textaera */
+  label: string;
 
-/** A typical, dynamically sized textarea that you are going to use in most forms */
-const TextArea = (props: TextAreaProps) => (
-  <AbstractInput as={TextareaAutosize} verticalAlign="top" {...props} />
-);
+  /** Whether the textarea has an invalid value */
+  invalid?: boolean;
+
+  /** Whether the textarea is required or not */
+  required?: boolean;
+
+  /** Whether the textarea is disabled or not */
+  disabled?: boolean;
+};
+
+const TextArea: React.FC<TextAreaProps> = ({
+  label,
+  invalid,
+  required,
+  disabled,
+  id,
+  name,
+  value,
+  ...rest
+}) => {
+  const identifier = id || name || slugify(label);
+
+  return (
+    <InputControl invalid={invalid} disabled={disabled} required={required}>
+      <InputElement as={TextareaAutosize} id={identifier} name={name} value={value} {...rest} />
+      <InputLabel raised={!!value} htmlFor={identifier}>
+        {label}
+      </InputLabel>
+    </InputControl>
+  );
+};
 
 export default TextArea;
--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import { ReactAttributes } from '../Box';
-import AbstractInput, { AbstractInputProps } from '../AbstractInput';
+import { slugify } from '../../utils/helpers';
+import { InputControl, InputElement, InputLabel } from '../utils/Input';
 
-export type TextInputProps = ReactAttributes<React.InputHTMLAttributes<HTMLInputElement>> &
-  Pick<AbstractInputProps, 'variant' | 'label' | 'invalid'>;
+export type TextInputProps = ReactAttributes<React.InputHTMLAttributes<HTMLInputElement>> & {
+  /** The label that is associated with this input */
+  label: string;
 
-const TextInput: React.FC<TextInputProps> = props => (
-  <AbstractInput as="input" type="text" truncated {...props} />
-);
+  /** Whether the input has an invalid value */
+  invalid?: boolean;
+
+  /** Whether the input is required or not */
+  required?: boolean;
+
+  /** Whether the input is disabled or not */
+  disabled?: boolean;
+};
+
+const TextInput: React.FC<TextInputProps> = ({
+  label,
+  invalid,
+  required,
+  disabled,
+  id,
+  name,
+  value,
+  ...rest
+}) => {
+  const identifier = id || name || slugify(label);
+
+  return (
+    <InputControl invalid={invalid} disabled={disabled} required={required}>
+      <InputElement
+        as="input"
+        type="text"
+        id={identifier}
+        name={name}
+        value={value}
+        truncated
+        {...rest}
+      />
+      <InputLabel raised={!!value} htmlFor={identifier}>
+        {label}
+      </InputLabel>
+    </InputControl>
+  );
+};
 
 export default React.memo(TextInput);
