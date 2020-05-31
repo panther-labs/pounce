48102a97f85c2d414967fb271eea66d63dfe20d8
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-05-31 17:53:32 +0300

    fix: add missing Heading styles

---
+++ src/components/Heading/useHeadingStyles.tsx
@@ -0,0 +1,24 @@
+import { BoxProps } from '../Box';
+import { HeadingProps } from './Heading';
+
+type UseHeadingStyles = Pick<HeadingProps, 'size'>;
+
+const useHeadingStyles = ({ size }: UseHeadingStyles): BoxProps => {
+  switch (size) {
+    case '2x-large':
+      return { fontSize: '6x-large' as const };
+    case 'x-large':
+      return { fontSize: '5x-large' as const };
+    case 'large':
+      return { fontSize: '4x-large' as const };
+    case 'medium':
+      return { fontSize: '3x-large' as const };
+    case 'small':
+      return { fontSize: '2x-large' as const };
+    case 'x-small':
+    default:
+      return { fontSize: 'x-large' as const };
+  }
+};
+
+export default useHeadingStyles;
