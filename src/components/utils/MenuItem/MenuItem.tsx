2fc78d8bbd13bd2332e01efe494156a297cd4409
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-06-01 09:25:57 +0300

    fix: remove `highlighted` & add better disabled support for `MenuItem`

--- src/components/utils/MenuItem/MenuItem.tsx
+++ src/components/utils/MenuItem/MenuItem.tsx
@@ -5,9 +5,6 @@ import { Theme } from '../../../theme';
 import { ReactAttributes } from '../../Box';
 
 interface MenuItemProps extends ReactAttributes<React.AllHTMLAttributes<HTMLElement>> {
-  /** Whether the current item is highlighted through the keyboard **/
-  highlighted?: boolean;
-
   /** Whether the current item is currently selected **/
   selected?: boolean;
 
@@ -22,19 +19,10 @@ interface MenuItemProps extends ReactAttributes<React.AllHTMLAttributes<HTMLElem
  * A MenuItem is simply an entry in a list of menu options or dropdown options. In general, this
  * should be used only on autocompletes, comboboxes & menus.
  */
-const MenuItem: React.FC<MenuItemProps> = ({
-  highlighted,
-  selected,
-  disabled,
-  children,
-  ...rest
-}) => {
+const MenuItem: React.FC<MenuItemProps> = ({ selected, disabled, children, ...rest }) => {
   let backgroundColor: keyof Theme['colors'] = 'navyblue-450';
   if (selected) {
     backgroundColor = 'navyblue-700';
-  }
-  if (highlighted) {
-    backgroundColor = 'navyblue-600';
   }
 
   return (
@@ -48,6 +36,12 @@ const MenuItem: React.FC<MenuItemProps> = ({
       transition="background-color 200ms cubic-bezier(0.0, 0, 0.2, 1) 0ms"
       position="relative"
       backgroundColor={backgroundColor}
+      aria-disabled={disabled}
+      disabled={disabled}
+      _disabled={disabledStyles}
+      _selected={{
+        backgroundColor: 'navyblue-600',
+      }}
       _after={{
         content: `url( 'data:image/svg+xml; utf8,<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 20 18" fill="white"><path d="M7 14.17L2.83 10l-1.41 1.41L7 17 19 5l-1.41-1.42L7 14.17z" /></svg>' )`,
         display: selected ? 'block' : 'none',
@@ -59,7 +53,6 @@ const MenuItem: React.FC<MenuItemProps> = ({
         bottom: 0,
         margin: 'auto 0',
       }}
-      {...(disabled && disabledStyles)}
       {...rest}
     >
       {children}
