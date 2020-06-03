3673a8abbed56c246c62c89c64b79038416898c9
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-06-03 16:49:38 +0300

    feat: add `variant` to Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import Box, { BoxProps } from '../Box';
 
-export type CardProps = BoxProps;
+export type CardProps = BoxProps & {
+  variant?: 'light' | 'dark';
+};
 
 /**
  * Extends <a href="/#/Box">Box</a>
  *
  * A card is simply a basic layout component with additional style props.
  */
-const Card: React.FC<CardProps> = React.forwardRef(function Card(props, ref) {
-  return <Box ref={ref} bg="navyblue-600" borderRadius="medium" {...props} />;
+const Card: React.FC<CardProps> = React.forwardRef(function Card(
+  { variant = 'light', ...rest },
+  ref
+) {
+  return (
+    <Box
+      ref={ref}
+      bg={variant === 'light' ? 'navyblue-600' : 'navyblue-700'}
+      borderRadius="medium"
+      {...rest}
+    />
+  );
 });
 
 export default Card;
