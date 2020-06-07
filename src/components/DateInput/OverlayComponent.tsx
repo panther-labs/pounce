b9e451dadd34da10d119057deeec19465cf2d928
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-06-07 12:30:48 +0300

    feat: add super basic fade-in to date input

--- src/components/DateInput/OverlayComponent.tsx
+++ src/components/DateInput/OverlayComponent.tsx
@@ -3,6 +3,7 @@ import Box from '../Box';
 import Card from '../Card';
 import Flex from '../Flex';
 import { IconButton } from '../../index';
+import FadeIn from '../FadeIn';
 
 interface CustomOverlayProps {
   selectedDay?: Date;
@@ -16,21 +17,23 @@ const OverlayComponent: React.FC<CustomOverlayProps> = ({
   ...rest
 }) => {
   return (
-    <Box position="relative" {...rest}>
-      <Card position="absolute" top={0} zIndex={100} p={6}>
-        <Flex align="center" justify="space-between" mb={4}>
-          <IconButton size="small" icon="arrow-back" aria-label="Go to previous page" />
-          <Box as="h4" fontSize="medium" fontWeight="bold">
-            {(selectedDay || month || new Date()).toLocaleString('default', {
-              month: 'long',
-              year: 'numeric',
-            })}
-          </Box>
-          <IconButton size="small" icon="arrow-forward" aria-label="Go to next page" />
-        </Flex>
-        {children}
-      </Card>
-    </Box>
+    <FadeIn from="bottom">
+      <Box position="relative" {...rest}>
+        <Card position="absolute" mt={4} top={0} zIndex={100} p={6}>
+          <Flex align="center" justify="space-between" mb={4}>
+            <IconButton size="small" icon="arrow-back" aria-label="Go to previous page" />
+            <Box as="h4" fontSize="medium" fontWeight="bold">
+              {(selectedDay || month || new Date()).toLocaleString('default', {
+                month: 'long',
+                year: 'numeric',
+              })}
+            </Box>
+            <IconButton size="small" icon="arrow-forward" aria-label="Go to next page" />
+          </Flex>
+          {children}
+        </Card>
+      </Box>
+    </FadeIn>
   );
 };
 
