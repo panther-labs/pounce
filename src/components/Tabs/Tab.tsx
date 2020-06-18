e6b0020bdbbe6c81bc656db4cd2314261db3c05d
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-06-18 14:28:13 +0300

    feat: remove styling from Tab

--- src/components/Tabs/Tab.tsx
+++ src/components/Tabs/Tab.tsx
@@ -1,35 +1,11 @@
 import React from 'react';
 import { Tab as ReachTab, TabProps as ReachTabProps } from '@reach/tabs';
-import AbstractButton from '../AbstractButton';
-import { addOpacity } from '../../utils/helpers';
-import useTheme from '../../utils/useTheme';
 
 export type TabProps = ReachTabProps;
 
 const Tab = React.forwardRef<HTMLButtonElement, TabProps>(function Tab({ children, ...rest }, ref) {
-  const theme = useTheme();
-  const activeColor = 'blue-600';
-  const focusBorderColor = addOpacity(theme.colors[activeColor], 0.1);
-
   return (
-    <ReachTab
-      as={AbstractButton}
-      ref={ref}
-      outline="none"
-      borderBottom="3px solid"
-      transition="border-color 300ms cubic-bezier(0.0, 0, 0.2, 1) 0ms"
-      borderColor="transparent"
-      _hover={{
-        borderColor: focusBorderColor,
-      }}
-      _focus={{
-        borderColor: focusBorderColor,
-      }}
-      _selected={{
-        borderColor: activeColor,
-      }}
-      {...rest}
-    >
+    <ReachTab ref={ref} {...rest}>
       {children}
     </ReachTab>
   );
