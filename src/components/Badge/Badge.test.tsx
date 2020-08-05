57a8180213a77bd0f0c788778804af59ea0637d8
vorillaz <theodorevorillas@gmail.com>
2020-08-05 21:05:59 +0300

    fix: add proper role for <Badge /> component and axe testing

---
+++ src/components/Badge/Badge.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, act, waitFor } from 'test-utils';
+import { AxeResults } from 'test-utils/types';
+import { axe } from 'jest-axe';
+
+import Badge from './Badge';
+
+it('renders', () => {
+  const { container } = render(
+    <>
+      <Badge color="red-300">INFO</Badge>
+      <Badge color="teal-500">LOW</Badge>
+      <Badge color="blue-400">WHATEVER</Badge>
+    </>
+  );
+  expect(container).toMatchSnapshot();
+});
+
+it('aligns with a11y', async () => {
+  const { container } = render(
+    <>
+      <Badge color="red-300">INFO</Badge>
+      <Badge color="teal-500">LOW</Badge>
+      <Badge color="blue-400">WHATEVER</Badge>
+    </>
+  );
+  let results: AxeResults;
+  await act(async () => {
+    results = await axe(container);
+  });
+  await waitFor(() => {
+    expect(results).toHaveNoViolations();
+  });
+});
