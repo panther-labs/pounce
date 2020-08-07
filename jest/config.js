0ffc33219fe57b0a72f660e7116b19323d3bf610
vorillaz <theodorevorillas@gmail.com>
2020-08-08 00:43:38 +0300

    feat: add Jest test coverage scripts

--- jest/config.js
+++ jest/config.js
@@ -3,7 +3,7 @@ const { defaults } = require('jest-config');
 module.exports = {
   rootDir: '../',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
-  testPathIgnorePatterns: ['/dist/', '/.github/', '/node_modules/'],
+  testPathIgnorePatterns: ['/dist/', '/.github/', '/node_modules/', 'jest'],
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
   setupFilesAfterEnv: ['<rootDir>/jest/setup.ts'],
   verbose: true,
