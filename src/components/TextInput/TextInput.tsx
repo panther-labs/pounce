8fb544a807280da3b55cabc4bd9257f6bb48eb61
vorillaz <theodorevorillas@gmail.com>
2020-08-08 00:23:47 +0300

    feat: extend TextInput with icons

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NativeAttributes } from '../Box';
 import { slugify } from '../../utils/helpers';
+import Icon, { IconProps } from '../Icon';
+import Flex from '../Flex';
 import { InputControl, InputElement, InputLabel } from '../utils/Input';
 
 export type TextInputProps = NativeAttributes<'input'> & {
@@ -15,29 +17,35 @@ export type TextInputProps = NativeAttributes<'input'> & {
 
   /** Whether the input is disabled or not */
   disabled?: boolean;
+
+  /** The icon present on the input  */
+  icon?: IconProps['type'];
 };
 
 const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(function TextInput(
-  { label, invalid, required, disabled, id, hidden, name, value, ...rest },
+  { label, invalid, required, disabled, id, hidden, name, icon, value, ...rest },
   ref
 ) {
   const identifier = id || name || slugify(label);
 
   return (
     <InputControl invalid={invalid} disabled={disabled} required={required} hidden={hidden}>
-      <InputElement
-        as="input"
-        type="text"
-        id={identifier}
-        name={name}
-        value={value}
-        truncated
-        {...rest}
-        ref={ref}
-      />
-      <InputLabel raised={!!value} htmlFor={identifier}>
-        {label}
-      </InputLabel>
+      <Flex align="center">
+        <InputElement
+          as="input"
+          type="text"
+          id={identifier}
+          name={name}
+          value={value}
+          truncated
+          {...rest}
+          ref={ref}
+        />
+        <InputLabel raised={!!value} htmlFor={identifier}>
+          {label}
+        </InputLabel>
+        {icon && <Icon size="small" mx={4} type={icon} />}
+      </Flex>
     </InputControl>
   );
 });
