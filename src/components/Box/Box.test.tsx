7d25e9274fb04c86c5387db4ee5c6a5811e8d443
vorillaz <theodorevorillas@gmail.com>
2020-08-07 11:54:13 +0300

    chore: cleanup <Box /> snapshot testing

--- src/components/Box/Box.test.tsx
+++ src/components/Box/Box.test.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { render } from 'test-utils';
 import Box from './Box';
 
-// FIXME: jest-emotion does not generate deterministic snapshots when the styling body is empty
-// https://github.com/emotion-js/emotion/issues/1960
 describe('Box', () => {
+  it('renders', async () => {
+    const { container } = await render(<Box />);
+    expect(container).toMatchSnapshot();
+  });
+
   it('allows truncated values', async () => {
     const { container } = await render(<Box truncated />);
     expect(container).toMatchSnapshot();
   });
-
   it('allows styling on demand', async () => {
     const { container } = await render(
       <Box color="black" background="red" textTransform="uppercase" />
