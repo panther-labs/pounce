2c567241710096e2303e81aad3851a2b439d1f63
vorillaz <theodorevorillas@gmail.com>
2020-08-25 19:10:05 +0300

    chore: extend ButtonProps to assign styled system values

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import AbstractButton from '../AbstractButton';
 import { BoxProps, NativeAttributes } from '../Box';
+import { SystemProps } from '../Box/system';
 import useButtonStyles from './useButtonStyles';
 import Spinner from '../Spinner';
 import Icon, { IconProps } from '../Icon';
 
-export interface ButtonProps extends NativeAttributes<'button'>, Pick<BoxProps, 'as' | 'to'> {
+export interface ButtonProps
+  extends NativeAttributes<'button'>,
+    SystemProps,
+    Pick<BoxProps, 'as' | 'to'> {
   /** The size (height) of the button */
   size?: 'small' | 'large';
 
