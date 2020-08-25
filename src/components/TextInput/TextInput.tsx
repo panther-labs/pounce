bf4332fe7c7b06649ee440d9fad726e35e74e109
vorillaz <theodorevorillas@gmail.com>
2020-08-25 11:13:15 +0300

    feat: add icon support for TextInput component

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { NativeAttributes } from '../Box';
 import { slugify } from '../../utils/helpers';
 import { InputControl, InputElement, InputLabel } from '../utils/Input';
+import Icon, { IconProps } from '../Icon';
+import Flex from '../Flex';
 
 export type TextInputProps = NativeAttributes<'input'> & {
   /** The label that is associated with this input */
@@ -15,29 +17,38 @@ export type TextInputProps = NativeAttributes<'input'> & {
 
   /** Whether the input is disabled or not */
   disabled?: boolean;
+
+  /** The icon present on the input  */
+  icon?: IconProps['type'];
 };
 
 const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(function TextInput(
-  { label, invalid, required, disabled, id, hidden, name, value, ...rest },
+  { label, invalid, required, disabled, id, hidden, name, icon, value, ...rest },
   ref
 ) {
   const identifier = id || name || slugify(label);
 
   return (
     <InputControl invalid={invalid} disabled={disabled} required={required} hidden={hidden}>
-      <InputElement
-        as="input"
-        type="text"
-        id={identifier}
-        name={name}
-        value={value}
-        truncated
-        {...rest}
-        ref={ref}
-      />
-      <InputLabel raised={!!value} htmlFor={identifier}>
-        {label}
-      </InputLabel>
+      <Flex align="center" position="relative">
+        <InputElement
+          as="input"
+          type="text"
+          id={identifier}
+          name={name}
+          value={value}
+          truncated
+          pr={icon && 10}
+          position="relative"
+          zIndex={2}
+          {...rest}
+          ref={ref}
+        />
+        <InputLabel raised={!!value} htmlFor={identifier}>
+          {label}
+        </InputLabel>
+        {icon && <Icon zIndex={1} position="absolute" size="small" right={4} type={icon} />}
+      </Flex>
     </InputControl>
   );
 });
