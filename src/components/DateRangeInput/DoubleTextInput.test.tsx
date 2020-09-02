f29c9a6dd7bc62462319f651e23df286c3f02738
vorillaz <theodorevorillas@gmail.com>
2020-09-02 17:05:37 +0300

    chore: enhance new DoubleTextInput specs

--- src/components/DateRangeInput/DoubleTextInput.test.tsx
+++ src/components/DateRangeInput/DoubleTextInput.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { renderWithTheme } from 'test-utils';
+import { renderWithTheme, fireEvent } from 'test-utils';
 import DoubleTextInput from './DoubleTextInput';
 
 const props = {
@@ -9,6 +9,13 @@ const props = {
 
 it('renders', async () => {
   const { container } = await renderWithTheme(<DoubleTextInput {...props} />);
+  expect(container).toMatchSnapshot();
+});
+
+it('renders with placeholders', async () => {
+  const { container } = await renderWithTheme(
+    <DoubleTextInput {...props} placeholderFrom="Foo" placeholderTo="Bar" />
+  );
   expect(container).toMatchSnapshot();
 });
 
@@ -25,6 +32,39 @@ it('renders with invalid option', async () => {
 it('renders with required option', async () => {
   const { container } = await renderWithTheme(<DoubleTextInput {...props} required />);
   expect(container).toMatchSnapshot();
+});
+
+it('renders with individual values', async () => {
+  const DoubleOrNothing = () => {
+    const [fromValue, setFrom] = React.useState('This is the start');
+    const [toValue, setTo] = React.useState('This is the end');
+    const onChangeFrom = React.useCallback(e => setFrom(e.currentTarget.value), [setFrom]);
+    const onChangeTo = React.useCallback(e => setTo(e.currentTarget.value), [setTo]);
+    return (
+      <DoubleTextInput
+        {...props}
+        from={fromValue}
+        to={toValue}
+        onChangeFrom={onChangeFrom}
+        onChangeTo={onChangeTo}
+        id="sample"
+      />
+    );
+  };
+  const { container, getByLabelText } = await renderWithTheme(<DoubleOrNothing />);
+  const fromInput = getByLabelText('Name');
+  const toInput = getByLabelText('Surname');
+
+  expect(fromInput.value).toBe('This is the start');
+  expect(toInput.value).toBe('This is the end');
+
+  expect(container).toMatchSnapshot();
+
+  fireEvent.change(fromInput, { target: { value: 'Another day another new start' } });
+  fireEvent.change(toInput, { target: { value: 'Change upon the end' } });
+
+  expect(fromInput.value).toBe('Another day another new start');
+  expect(toInput.value).toBe('Change upon the end');
 });
 
 it('renders with icons', async () => {
