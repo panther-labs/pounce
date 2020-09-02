553c8e5dbcef4009c501ce873e7795f7a8683fdf
vorillaz <theodorevorillas@gmail.com>
2020-09-02 12:45:16 +0300

    fix: change preset list semantics

--- src/components/DateRangeInput/Presets.tsx
+++ src/components/DateRangeInput/Presets.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Dayjs } from 'dayjs';
 import { getDates } from '../../utils/helpers';
 import Box from '../Box';
+import AbstractButton from '../AbstractButton';
 
 type OnSelectCallback = (date: [Dayjs, Dayjs]) => void;
 type OnCurrentMonthSelect = React.Dispatch<React.SetStateAction<Dayjs>>;
@@ -13,22 +14,21 @@ interface PresetsProps {
 
 interface ListItemProps {
   selected?: boolean;
-  onSelect: (evt: React.KeyboardEvent) => void;
+  onSelect: (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 const ListItem: React.FC<ListItemProps> = ({ selected, onSelect, ...rest }) => (
-  <Box
-    as="li"
-    mb={6}
-    cursor="pointer"
-    color="navyblue-100"
-    _selected={{ color: 'gray-50' }}
-    _hover={{ color: 'gray-50' }}
-    aria-selected={selected}
-    onClick={onSelect}
-    fontSize="medium"
-    {...rest}
-  />
+  <Box as="li" mb={6}>
+    <AbstractButton
+      color="navyblue-100"
+      _selected={{ color: 'gray-50' }}
+      _hover={{ color: 'gray-50' }}
+      aria-selected={selected}
+      onClick={onSelect}
+      fontSize="medium"
+      {...rest}
+    ></AbstractButton>
+  </Box>
 );
 
 const options = [
@@ -36,7 +36,7 @@ const options = [
     id: 'last_day',
     label: 'Last 24 Hours',
     onSelectPreset: (callback: OnSelectCallback, setCurrentMonth: OnCurrentMonthSelect) => (
-      e: React.KeyboardEvent
+      e: React.MouseEvent
     ): void => {
       e.preventDefault();
       const { lastDay, nextMonth, now } = getDates();
@@ -48,7 +48,7 @@ const options = [
     id: 'last_week',
     label: 'Last Week',
     onSelectPreset: (callback: OnSelectCallback, setCurrentMonth: OnCurrentMonthSelect) => (
-      e: React.KeyboardEvent
+      e: React.MouseEvent
     ): void => {
       e.preventDefault();
       const { lastWeek, nextMonth, now } = getDates();
@@ -60,7 +60,7 @@ const options = [
     id: 'last_month',
     label: 'Last Month',
     onSelectPreset: (callback: OnSelectCallback, setCurrentMonth: OnCurrentMonthSelect) => (
-      e: React.KeyboardEvent
+      e: React.MouseEvent
     ): void => {
       e.preventDefault();
       const { lastMonth, nextMonth, now } = getDates();
@@ -72,7 +72,7 @@ const options = [
     id: 'last_three_months',
     label: 'Last 3 Months',
     onSelectPreset: (callback: OnSelectCallback, setCurrentMonth: OnCurrentMonthSelect) => (
-      e: React.KeyboardEvent
+      e: React.MouseEvent
     ): void => {
       e.preventDefault();
       const { lastThreeMonths, nextMonth, now } = getDates();
@@ -84,7 +84,7 @@ const options = [
     id: 'last_six_months',
     label: 'Last 6 Months',
     onSelectPreset: (callback: OnSelectCallback, setCurrentMonth: OnCurrentMonthSelect) => (
-      e: React.KeyboardEvent
+      e: React.MouseEvent
     ): void => {
       e.preventDefault();
       const { lastSixMonths, nextMonth, now } = getDates();
@@ -95,7 +95,7 @@ const options = [
   {
     id: 'custom',
     label: 'Custom',
-    onSelectPreset: () => (e: React.KeyboardEvent) => {
+    onSelectPreset: () => (e: React.MouseEvent) => {
       e.preventDefault();
     },
   },
@@ -136,7 +136,7 @@ const Presets: React.FC<PresetsProps> = ({ currentDateRange, onSelect, setCurren
   return (
     <Box borderRight="1px solid" borderColor="navyblue-300">
       <Box p={6} width="140px">
-        <Box as="ul">
+        <Box as="ol">
           {options.map(opt => (
             <ListItem
               onSelect={opt.onSelectPreset(onSelect, setCurrentMonth)}
