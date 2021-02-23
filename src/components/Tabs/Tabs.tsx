206b71e76c96e13ca77fb0823799e94220e8ffac
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2021-02-23 18:38:40 +0200

    fix: broken tabs export

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,4 +1,7 @@
-import { Tabs } from '@reach/tabs';
+import { Tabs as ReachTabs, TabsProps as ReachTabsProps, TabsOrientation } from '@reach/tabs';
 
-export { Tabs, TabsProps, TabsOrientation } from '@reach/tabs';
+export const Tabs = ReachTabs;
+export type TabsProps = ReachTabsProps;
+export { TabsOrientation };
+
 export default Tabs;
