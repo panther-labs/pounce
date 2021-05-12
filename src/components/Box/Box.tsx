6acf49fa6cf60cc536908808d495e3568198d8d2
kouknick <nick.koukounakis@runpanther.io>
2021-05-12 15:56:02 +0300

    chore: wip styles

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -11,8 +11,52 @@ export type { NativeAttributes };
  * it also supports all the native HTML attributes.
  * */
 // @ts-nocheck
-const Box: React.FC<BoxProps> = ({ children, as: As }) => {
-  return <As>{children}</As>;
+const Box: React.FC<BoxProps> = ({
+  children,
+  as: As = 'div',
+  position,
+  display,
+  m,
+  mx,
+  my,
+  ml,
+  mr,
+  mb,
+  mt,
+  px,
+  py,
+  pl,
+  pr,
+  pt,
+  pb,
+  p,
+}) => {
+  const marginLeft = ml || mx || m || 0;
+  const marginRight = mr || mx || m || 0;
+  const marginTop = mt || my || m || 0;
+  const marginBottom = mb || my || m || 0;
+  const paddingLeft = pl || px || p || 0;
+  const paddingRight = pr || px || p || 0;
+  const paddingTop = pt || py || p || 0;
+  const paddingBottom = pb || py || p || 0;
+  return (
+    <As
+      style={{
+        position,
+        display,
+        marginLeft,
+        marginRight,
+        marginTop,
+        marginBottom,
+        paddingLeft,
+        paddingRight,
+        paddingTop,
+        paddingBottom,
+      }}
+    >
+      {children}
+    </As>
+  );
 };
 
 // FIXME: This overrides the fact that the box is a `div`. Components implementing this will warn us
