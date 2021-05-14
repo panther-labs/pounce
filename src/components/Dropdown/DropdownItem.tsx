3ecb65d6e2030176251607e8d7feabddad3a30b3
Alex <alex.a.mylonas@gmail.com>
2021-05-14 08:17:26 +0300

    chore: bump reach to 0.15

--- src/components/Dropdown/DropdownItem.tsx
+++ src/components/Dropdown/DropdownItem.tsx
@@ -1,18 +1,20 @@
-import { forwardRefWithAs } from '@reach/utils';
 import { MenuItem as ReachMenuItem, MenuLinkProps as ReachMenuLinkProps } from '@reach/menu-button';
 import MenuItem from '../utils/MenuItem/MenuItem';
 import React from 'react';
 
 export type DropdownItemProps = ReachMenuLinkProps; // this is not a typo, it's intentional
 
-export const DropdownItem = forwardRefWithAs<DropdownItemProps & { disabled?: boolean }, 'div'>(
-  function DropdownItem({ children, onSelect = () => {}, disabled, ...rest }, ref) {
-    return (
-      <ReachMenuItem disabled={disabled} onSelect={onSelect} ref={ref} {...rest}>
-        <MenuItem disabled={disabled}>{children}</MenuItem>
-      </ReachMenuItem>
-    );
-  }
-);
+export const DropdownItem: React.FC<DropdownItemProps & { disabled?: boolean }> = ({
+  children,
+  onSelect = () => {},
+  disabled,
+  ...rest
+}) => {
+  return (
+    <ReachMenuItem disabled={disabled} onSelect={onSelect} as="div" {...rest}>
+      <MenuItem disabled={disabled}>{children}</MenuItem>
+    </ReachMenuItem>
+  );
+};
 
 export default DropdownItem;
--- src/components/Dropdown/DropdownLink.tsx
+++ src/components/Dropdown/DropdownLink.tsx
@@ -1,18 +1,19 @@
-import { forwardRefWithAs } from '@reach/utils';
 import { MenuLink as ReachMenuLink, MenuLinkProps as ReachMenuLinkProps } from '@reach/menu-button';
 import MenuItem from '../utils/MenuItem/MenuItem';
 import React from 'react';
 
 export type DropdownLinkProps = ReachMenuLinkProps;
 
-export const DropdownLink = forwardRefWithAs<DropdownLinkProps & { disabled?: boolean }, 'a'>(
-  function DropdownLink({ children, disabled, ...rest }, ref) {
-    return (
-      <ReachMenuLink disabled={disabled} ref={ref} {...rest}>
-        <MenuItem disabled={disabled}>{children}</MenuItem>
-      </ReachMenuLink>
-    );
-  }
-);
+export const DropdownLink: React.FC<DropdownLinkProps & { disabled?: boolean }> = ({
+  children,
+  disabled,
+  ...rest
+}) => {
+  return (
+    <ReachMenuLink disabled={disabled} as="a" {...rest}>
+      <MenuItem disabled={disabled}>{children}</MenuItem>
+    </ReachMenuLink>
+  );
+};
 
 export default DropdownLink;
