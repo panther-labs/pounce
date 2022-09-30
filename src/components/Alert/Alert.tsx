f42dfd8bac8fed9dc8f0b607a7af737daa8067fe
jg-panther <109993923+jg-panther@users.noreply.github.com>
2022-09-30 11:21:46 -0700

    refactor: adding disableAnimation prop to Alert

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import ControlledAlert, { ControlledAlertProps } from '../utils/ControlledAlert';
 import Collapse from '../Collapse';
-import omit from 'lodash/omit';
-import { noop } from '../../utils/helpers';
 
 export type AlertProps = Omit<ControlledAlertProps, 'open' | 'onClose'> & {
   onClose?: () => void;
+
+  /** Whether the Alert is wrapped in a Collapse animation wrapper */
   disableAnimation?: boolean;
 };
 
 /** An Alert component is simply a container for text that should capture the user's attention */
 const Alert: React.FC<AlertProps> = ({ onClose, disableAnimation = false, ...rest }) => {
   const [open, setOpen] = React.useState(true);
-  // Remove the discardable prop when the disableAnimation prop is true
-  const props = disableAnimation ? omit(rest, 'discardable') : rest;
 
   const handleClose = React.useCallback(() => {
     setOpen(false);
@@ -24,10 +22,10 @@ const Alert: React.FC<AlertProps> = ({ onClose, disableAnimation = false, ...res
   }, [setOpen, onClose]);
 
   return disableAnimation ? (
-    <ControlledAlert {...props} open onClose={noop} />
+    <ControlledAlert {...rest} open={open} onClose={handleClose} />
   ) : (
     <Collapse open={open}>
-      <ControlledAlert {...props} open onClose={handleClose} />
+      <ControlledAlert {...rest} open onClose={handleClose} />
     </Collapse>
   );
 };
