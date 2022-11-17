6afaf6daa32e4bddc3c97cea427040bbac7deb88
AmandaO <amanda.olczak@panther.io>
2022-11-16 19:31:39 -0800

    feat: allow breadcrumbs to be truncated when more than 3 items exist

---
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import Breadcrumbs from './index';
+import { renderWithTheme } from '../../../jest/utils';
+
+const mockCrumbs = [
+  {
+    children: 'First page',
+    href: '/',
+  },
+  {
+    children: 'Second page',
+    href: '/secondpage',
+  },
+  {
+    children: 'Third page',
+    href: '/thirdpage',
+  },
+  {
+    children: 'Fourth page',
+    href: '/fourthpage',
+  },
+  {
+    children: 'Fifth page',
+    href: '/fifthpage',
+  },
+  {
+    children: 'Sixth page',
+    href: '/sixthpage',
+  },
+  {
+    children: 'Seventh page',
+    href: '/seventhpage',
+  },
+];
+const truncatedCrumbs = mockCrumbs.slice(1, -2);
+const visibleCrumbs = mockCrumbs.filter(index => {
+  return truncatedCrumbs.indexOf(index) === -1;
+});
+
+describe('Breadcrumbs', () => {
+  it('renders all items without truncation', () => {
+    const { queryAllByRole, queryByLabelText, queryByText } = renderWithTheme(
+      <Breadcrumbs items={mockCrumbs} />
+    );
+
+    // verify expected breadcrumbs and chevrons exist
+    mockCrumbs.forEach(crumb => {
+      expect(queryByText(crumb.children)).toBeTruthy();
+    });
+    expect(queryAllByRole('presentation')).toHaveLength(mockCrumbs.length - 1);
+
+    // verify truncated menu does not exist
+    expect(queryByLabelText('Truncated breadcrumb list toggle')).toBeNull();
+  });
+
+  it('renders breadcrumbs with truncation', () => {
+    const { queryAllByRole, queryByLabelText, queryByText } = renderWithTheme(
+      <Breadcrumbs items={mockCrumbs} truncate />
+    );
+
+    visibleCrumbs.forEach(crumb => {
+      expect(queryByText(crumb.children)).toBeTruthy();
+    });
+    truncatedCrumbs.forEach(crumb => {
+      expect(queryByText(crumb.children)).toBeNull();
+    });
+    expect(queryAllByRole('presentation')).toHaveLength(3);
+    expect(queryByLabelText('Truncated breadcrumb list toggle')).toBeTruthy();
+  });
+
+  it('renders breadcrumbs without truncation when list length is less than 4', () => {
+    const { queryAllByRole, queryByLabelText, queryByText } = renderWithTheme(
+      <Breadcrumbs items={visibleCrumbs} truncate />
+    );
+
+    // verify expected breadcrumbs and chevrons exist
+    visibleCrumbs.forEach(crumb => {
+      expect(queryByText(crumb.children)).toBeTruthy();
+    });
+    expect(queryAllByRole('presentation')).toHaveLength(visibleCrumbs.length - 1);
+
+    // verify truncated menu does not exist
+    expect(queryByLabelText('Truncated breadcrumb list toggle')).toBeNull();
+  });
+});
--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Box from '../Box';
+import Button from '../Button';
+import Card from '../Card';
 import Flex from '../Flex';
 import Icon from '../Icon';
-import Box from '../Box';
 import Link, { LinkProps } from '../Link';
+import { Popover, PopoverContent, PopoverTrigger } from '../Popover';
 
 export interface BreadcrumbItem {
   /** The URL that this Breadcrumbs should navigate to when clicked */
@@ -16,48 +19,135 @@ export interface BreadcrumbItem {
 export interface BreadcrumbProps extends Pick<LinkProps, 'as'> {
   /** A list of `BreadcrumbsItem` objects ( `{href,children}` ) that will construct the Breadcrumb */
   items: BreadcrumbItem[];
+  /** Truncates component if breadcrumb list is too long.
+   * At least 4 breadcrumbs must exist for truncation to occur.
+   */
+  truncate?: boolean;
 }
 
+interface IBreadcrumbLink {
+  crumb: BreadcrumbItem;
+  isLastBreadcrumb?: boolean;
+  showChevron?: boolean;
+}
+
+type ITruncatedBreadcrumbs = Omit<BreadcrumbProps, 'truncate'>;
+
+const BreadcrumbChevron: React.ElementType = () => {
+  return <Icon type="chevron-right" mx={2} color="blue-300" size="medium" role="presentation" />;
+};
+
+const BreadcrumbLink: React.ElementType = ({
+  crumb,
+  isLastBreadcrumb = false,
+  showChevron = true,
+  ...rest
+}: IBreadcrumbLink) => {
+  return (
+    <Flex
+      key={crumb.href}
+      as="li"
+      alignItems="center"
+      fontSize="medium"
+      fontWeight={isLastBreadcrumb ? 'bold' : 'normal'}
+      aria-current={isLastBreadcrumb ? 'page' : undefined}
+    >
+      <Link
+        to={crumb.href}
+        variant="neutral"
+        border="none"
+        truncated
+        maxWidth="450px"
+        data-active={isLastBreadcrumb ? true : undefined}
+        {...rest}
+      >
+        {crumb.children}
+      </Link>
+      {!isLastBreadcrumb && showChevron && <BreadcrumbChevron />}
+    </Flex>
+  );
+};
+
+const TruncatedBreadcrumbs: React.ElementType = ({ items, ...rest }: ITruncatedBreadcrumbs) => {
+  const firstItem = items[0];
+  const truncatedList = items.slice(1, -2);
+  const remainingCrumbs = items.slice(-2);
+
+  return (
+    <React.Fragment>
+      {/* first crumb */}
+      <BreadcrumbLink crumb={firstItem} {...rest} />
+
+      {/* truncated crumbs */}
+      <Popover>
+        {() => (
+          <Flex alignItems="center">
+            <PopoverTrigger
+              as={Button}
+              id="truncated-breadcrumb-list"
+              variantColor="transparent"
+              padding={0}
+              height="auto"
+              aria-label="Truncated breadcrumb list toggle"
+            >
+              ...
+            </PopoverTrigger>
+            <PopoverContent alignment="bottom-right">
+              <Card as="ul" my={1} px={5} pt={5} shadow="dark300" minWidth={180}>
+                {truncatedList.map(item => {
+                  return (
+                    <Card key={item.href} minWidth={180} pb={5}>
+                      <BreadcrumbLink crumb={item} showChevron={false} {...rest} />
+                    </Card>
+                  );
+                })}
+              </Card>
+            </PopoverContent>
+            <BreadcrumbChevron />
+          </Flex>
+        )}
+      </Popover>
+
+      {/* remaining crumbs */}
+      {remainingCrumbs.map((item, index) => {
+        const isLastBreadcrumb = index === 1;
+        return (
+          <BreadcrumbLink
+            key={item.href}
+            crumb={item}
+            isLastBreadcrumb={isLastBreadcrumb}
+            {...rest}
+          />
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
 /** Breadcrumb is a way to navigate back to where you came from within the app */
-const Breadcrumbs: React.FC<BreadcrumbProps> = ({ items, ...rest }) => {
+const Breadcrumbs: React.FC<BreadcrumbProps> = ({ items, truncate = false, ...rest }) => {
+  const truncateBreadcrumbs = truncate && items.length > 3;
+
   return (
     <Box as="nav" aria-label="Breadcrumbs">
       <Flex as="ol">
-        {items.map((item, index) => {
-          const isLastBreadcrumb = index === items.length - 1;
-
-          return (
-            <Flex
-              key={item.href}
-              as="li"
-              alignItems="center"
-              fontSize="medium"
-              fontWeight={isLastBreadcrumb ? 'bold' : 'normal'}
-              aria-current={isLastBreadcrumb ? 'page' : undefined}
-            >
-              <Link
-                to={item.href}
-                variant="neutral"
-                border="none"
-                truncated
-                maxWidth="450px"
-                data-active={isLastBreadcrumb ? true : undefined}
-                {...rest}
-              >
-                {item.children}
-              </Link>
-              {!isLastBreadcrumb && (
-                <Icon
-                  type="chevron-right"
-                  mx={2}
-                  color="blue-300"
-                  size="medium"
-                  role="presentation"
+        {truncateBreadcrumbs ? (
+          <TruncatedBreadcrumbs items={items} {...rest} />
+        ) : (
+          <React.Fragment>
+            {items.map((item, index) => {
+              const isLastBreadcrumb = index === items.length - 1;
+              return (
+                <BreadcrumbLink
+                  key={item.href}
+                  crumb={item}
+                  isLastBreadcrumb={isLastBreadcrumb}
+                  {...rest}
                 />
-              )}
-            </Flex>
-          );
-        })}
+              );
+            })}
+          </React.Fragment>
+        )}
       </Flex>
     </Box>
   );
